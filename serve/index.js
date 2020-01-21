b76c5e4a7236fb38031bb6d4bc453faffe6648a8
yangguobao <yangguobao@edaijia-inc.cn>
2020-01-21 17:26:57 +0800

    update

--- serve/index.js
+++ serve/index.js
@@ -4,23 +4,18 @@ const logger = require('koa-logger');
 const static = require('koa-static');
 const chalk = require('chalk');
 const view = require('koa-view');
-const router = require('koa-router')();
+const router = require('../routes/')
 
 const app = new koa();
 app.use(logger());
 // 静态资源文件
 app.use(static(path.resolve(__dirname + '/..')));
 
-
 app.use(view(__dirname + '/../views', {
 	map: {
 		html: 'ejs'
 	}
 }));
-
-router.get('/', async (ctx, next) => {
-	ctx.render('index')
-});
 
 app.use(router.routes());
 
