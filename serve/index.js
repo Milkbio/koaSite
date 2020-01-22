c60e53fbc06c3731eb175e68f4fe176f56c9a0bb
yangguobao <yangguobao@edaijia-inc.cn>
2020-01-22 17:23:15 +0800

    添加webpack 热更新-----待完善

--- serve/index.js
+++ serve/index.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const koa = require('koa');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('koa-webpack-dev-middleware');
+const webpackHotMiddleware = require('koa-webpack-hot-middleware');
 const logger = require('koa-logger');
-const static = require('koa-static');
+const koastatic = require('koa-static');
 const chalk = require('chalk');
 const views = require('koa-views');
-const router = require('../routes/')
+const router = require('../routes/');
+const webpackConfig = require('./webpack.conf');
+
+const compiler = webpack(webpackConfig);
+
+const wdm = webpackDevMiddleware(compiler, {
+	noInfo: true,
+	//publicPath: config.output.publicPath
+})
 
 const app = new koa();
+app.use(wdm)
+app.use(webpackHotMiddleware(compiler))
 app.use(logger());
 
 // 静态资源文件
-app.use(static(path.resolve(__dirname + '/..')));
+app.use(koastatic(path.resolve(__dirname + '/..')));
 
 // 设置模板
 app.use(views(__dirname + '/../views', {
